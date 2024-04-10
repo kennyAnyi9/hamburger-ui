a3d06e665594781615b96997c16bf0e98a3e3089
kennyAnyi9 <kennyanyi@gmail.com>
2024-04-10 05:49:15 +0000

    fix: make site responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <SwitchDemo />
-      <h1 className="font-semibold text-opacity-50 text-6xl text-transparent bg-clip-text bg-gradient-to-t from-gray-950 to-gray-500">
+      <h1 className="font-semibold text-opacity-50 text-3xl text-center md:text-6xl text-transparent bg-clip-text bg-gradient-to-t from-gray-950 to-gray-500">
         Humburger UI
       </h1>
     </main>
