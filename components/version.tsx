ca22db7b23aad30899e2323ff98b0f2b45729ef4
kennyAnyi9 <kennyanyi@gmail.com>
2024-04-12 16:45:11 +0000

    fix bug

--- components/version.tsx
+++ components/version.tsx
@@ -26,9 +26,9 @@ export function Version() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                strokeidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <rect width="14" height="6" x="5" y="12" rx="2" />
                 <rect width="10" height="6" x="7" y="2" rx="2" />
