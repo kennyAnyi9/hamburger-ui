03e677a6e2f861a00c03195b322adb642df1b4b9
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-20 12:21:59 +0000

    feat: build sidebar menus

--- app/(docs)/_components/hamburgers/canvas-menu-right.tsx
+++ app/(docs)/_components/hamburgers/canvas-menu-right.tsx
@@ -1,41 +1,56 @@
 "use client";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import React, { SVGAttributes, useState } from "react";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { motion, useReducedMotion } from "framer-motion";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-import { motion, useReducedMotion } from "framer-motion";
-
+// Define the type for a navigation link
 interface NavigationLink {
   href: string;
   text: string;
 }
+
+// Define the type for the hamburger menu props
+interface HamburgerMenuProps {
+  isMenuOpen: boolean;
+  onToggleMenuOpen: () => void;
+}
+
 /* start by raplacing these with your menu items and links */
 const menuNavigationLinks: NavigationLink[] = [
-  { href: "/", text: "Homeward Bound" },
-  { href: "/docs", text: "Doc's Orders" },
-  { href: "/about", text: "The Big Reveal" },
-  { href: "/services", text: "We Got You Covered" },
-  { href: "/blog", text: "Fresh News & Brews ☕️" },
-  { href: "/contact", text: "Let's Chat! " },
-  { href: "/portfolio", text: "Showtime! ✨" },
-  { href: "/team", text: "Meet the Masterminds " },
+  { href: "/dashboard", text: "Homeward Bound" },
+  { href: "/", text: "Doc's Orders" },
+  { href: "#", text: "The Big Reveal" },
+  { href: "#", text: "We Got You Covered" },
+  { href: "#", text: "Fresh News & Brews ☕️" },
+  { href: "#", text: "Let's Chat! " },
+  { href: "#", text: "Showtime! ✨" },
+  { href: "#", text: "Meet the Masterminds " },
   // ... other links
 ];
 
-export default function CanvasMenuRight() {
+// Define the HamburgerMenu component
+const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
+  isMenuOpen,
+  onToggleMenuOpen,
+}) => {
   const isMotionReduced = useReducedMotion();
+
+  const handleLinkClick = () => {
+    onToggleMenuOpen();
+  };
+
   return (
-    <Sheet>
-      <SheetTrigger>
-        <MenuJustified />
-      </SheetTrigger>
-      <SheetContent side={"right"}>
-        <Link href="/">
+    <Sheet open={isMenuOpen} onOpenChange={onToggleMenuOpen}>
+      <SheetContent className="" side={"right"}>
+        <Link href="/" onClick={handleLinkClick}>
           <h1 className="text-lg inline-flex justify-center items-center gap-2 font-normal text-gray-800 dark:text-gray-200">
             {" "}
-            <span className="inline-flex justify-center items-center font-bold rounded-lg py-0.5 px-2 text-gray-100 dark:text-gray-800  bg-black dark:bg-white ">
+            <span className="inline-flex justify-center items-center font-bold rounded-lg py-0.5 px-2 text-gray-100 dark:text-gray-800  bg-black dark:bg-white">
               UI
             </span>
-            Canvas Menu
+            Menu
           </h1>
         </Link>
         <motion.div
@@ -69,6 +84,9 @@ export default function CanvasMenuRight() {
                   key={href}
                   className="text-lg font-semibold text-foreground hover:text-foreground/80"
                   href={href}
+                  onClick={() => handleLinkClick()}
+
+                  // target={target}
                 >
                   {text}
                 </Link>
@@ -79,26 +97,68 @@ export default function CanvasMenuRight() {
       </SheetContent>
     </Sheet>
   );
-}
+};
 
-const MenuJustified = ({ className, ...rest }: any) => {
+// Define the Hamburger component
+const Hamburger: React.FC<HamburgerMenuProps> = ({
+  isMenuOpen,
+  onToggleMenuOpen,
+}) => {
+  return (
+    <div className="flex lg:hidden">
+      <Button
+        variant="outline"
+        className="z-20 w-10 border-transparent p-0"
+        onClick={onToggleMenuOpen}
+      >
+        {isMenuOpen ? null : <PanelLeftClose />}
+      </Button>
+    </div>
+  );
+};
+// Define the MenuIcon component
+const PanelLeftClose: React.FC = (props: SVGAttributes<SVGSVGElement>) => {
   return (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className={className}
-      {...rest}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="size-6"
     >
-      <line x1="3" x2="21" y1="6" y2="6" />
-      <line x1="3" x2="21" y1="12" y2="12" />
-      <line x1="3" x2="21" y1="18" y2="18" />
+      <rect width="18" height="18" x="3" y="3" rx="2" />
+      <path d="M15 3v18" />
+      <path d="m10 15-3-3 3-3" />
     </svg>
   );
 };
+
+// Define the BasicMenu component
+const RightSideBar: React.FC = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenuOpen = () => {
+    setMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+  };
+
+  return (
+    <>
+      <Hamburger
+        isMenuOpen={isMenuOpen}
+        onToggleMenuOpen={handleToggleMenuOpen}
+      />
+      <HamburgerMenu
+        isMenuOpen={isMenuOpen}
+        onToggleMenuOpen={handleToggleMenuOpen}
+      />
+    </>
+  );
+};
+
+export default RightSideBar;
