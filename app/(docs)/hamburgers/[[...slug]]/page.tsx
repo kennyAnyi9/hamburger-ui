9c105a570eeff7a84c4385c4874bca93674da65d
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-20 11:18:06 +0000

    update branch

---
+++ app/(docs)/hamburgers/[[...slug]]/page.tsx
@@ -0,0 +1,32 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  BasicMenu,
+  CodePreviewSnippet,
+  CodePreviewSwitcher,
+  Configuration,
+  Introduction,
+} from "@/lib/exports";
+import { cn } from "@/lib/utils";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import Usage from "../../_components/docs/component-usage";
+
+export default function page() {
+  return (
+    <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <div className="mx-auto w-full min-w-0">
+        <div className="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
+          <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+            Docs
+          </div>
+          <ChevronRightIcon className="h-4 w-4" />
+          <div className="font-medium text-foreground">Basic Menu</div>
+        </div>
+        {/* <Configuration /> */}
+        {/* <Introduction /> */}
+        <Usage />
+      </div>
+    </main>
+  );
+}
