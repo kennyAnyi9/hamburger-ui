7c28566ec3c3ce4828fa93f3aee719a6c12fc4df
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-22 22:34:52 +0000

    conifure stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+};
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
