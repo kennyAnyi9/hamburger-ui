2d2365089a92678f0f7d7198e3eff5432c0a548e
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-22 22:35:13 +0000

    configure stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,10 @@ import defaultTheme, { fontFamily } from "tailwindcss/defaultTheme";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx,md,mdx}",
+    "./components/**/*.{ts,tsx,md,mdx}",
+    "./app/**/*.{ts,tsx,md,mdx}",
+    "./src/**/*.{ts,tsx,md,mdx}",
   ],
   prefix: "",
   theme: {
@@ -118,6 +118,7 @@ const config = {
         },
       },
     }),
+    // require("@tailwindcss/typography"),
     require("tailwindcss-animate"),
   ],
 } satisfies Config;
